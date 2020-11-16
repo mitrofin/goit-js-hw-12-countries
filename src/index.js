ffa770829f53b4749fd23f9e511c11e745eef49e
mitrofin <mitrofn@gmail.com>
2020-11-16 22:56:40 +0200

    final

--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ import '@pnotify/core/dist/BrightTheme.css';
 import '@pnotify/core/dist/PNotify.css';
 /* import { error } from '@pnotify/core'; */
 
-const { alert ,info, error } = require('@pnotify/core');
+const { alert, info, error } = require('@pnotify/core');
+error.delay = '1000';
+error.autoOpen = false; 
 
 const debounce = require('debounce');
 fetchCountries()
@@ -31,22 +33,17 @@ function onSearch(event) {
                 "Спробуйте ще раз!",
         });
         return;
-    }
-    /* const url = `https://restcountries.eu/rest/v2/name/${searchData}`;
-    fetch(url)
-        .then(response => {
-        return response.json();
-        }) */
+    } 
     fetchCountries(searchData)
             
         .then(country => {
         if (country.length > 10) {
-            showError()
+            showAlert()()
             clearCounrty() 
         
         }
-        if (country === undefined) {
-        showAlert()
+         if (country.status === 404)  {
+        showError()
         }
         console.log(country.length);
     
@@ -59,13 +56,7 @@ function onSearch(event) {
     
 }
 
-function clearResult() {
-        refs.searchForm.value = "";
-    }
 
-function clearCounrty() {
-        refs.containerList.innerHTML = "";
-}
     
 function appendCountries(country) {
     const markUp = countryListTmpl(country);
@@ -81,36 +72,40 @@ function appendCountries(country) {
         return (refs.containerList.insertAdjacentHTML('beforeend', markUpName))
     }
 
-    if (country.length > 10) {
+    /* if (country.length > 10) {
         clearCounrty()
+        showAlert() 
+    
     }
 
     if (country === undefined) {
-    showAlert()
-  }
+    showError()
+  } */
     
-    clearResult();
-    clearCounrty();
 
 }
 
 function showError() {
     const myError = error({
     text:
-            "Багато співпадінь.Уточіть пошук! Використовуйте ENG ", 
+            "Нічого не знайдено! ", 
     });
 
 }
 function showAlert() {
     const myAlert = alert({
     text:
-            "Нічого не знайдено!", 
+            "Багато співпадінь.Уточіть пошук! Використовуйте ENG", 
     });
+    
 
 }
+function clearResult() {
+        refs.searchForm.value = "";
+    }
 
-/* const myError = error({
-    text:
-      "Багато співпадінь. Уточіть пошук!" 
-  }); */
-      
+function clearCounrty() {
+        refs.containerList.innerHTML = "";
+}
+
+
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,12 +1,30 @@
  /* import countryListTmpl from '..templates/country-list.hbs'; */
+/*  import '@pnotify/core/dist/BrightTheme.css';
+import '@pnotify/core/dist/PNotify.css';
+const { error } = require('@pnotify/core');
+error.delay = '1000';
+error.autoOpen = false;  */
+
+
 
 export default
-    function fetchCountries(name) {
-        return fetch(`https://restcountries.eu/rest/v2/name/${name}`)
+    function fetchCountries(response) {
+        return fetch(`https://restcountries.eu/rest/v2/name/${response}`)
             .then(response => {
-                /* if (response.ok || response.status == 404) return response.json();
-                throw new Error('Error fetching data'); */
+                 if (response.ok)  return response.json();
+                else if (response.status === 404) {
+                /* showError(); */
                 return response.json();
+                }
+                /* return response.json(); */
             })
     
     }
+
+
+/* function showError() {
+     error({
+        text:
+            "Уточіть пошук! Використовуйте ENG ",
+    });
+} */
